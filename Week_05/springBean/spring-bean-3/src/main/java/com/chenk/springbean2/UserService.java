e8761767a9ce9bd641006569ff538ad6d48f51d1
chenk-ss <1510687833@qq.com>
2020-11-18 22:36:47 +0800

    写代码实现 Spring Bean 的装配，方式越多越好（XML、Annotation 都可以）

---
+++ Week_05/springBean/spring-bean-3/src/main/java/com/chenk/springbean2/UserService.java
@@ -0,0 +1,21 @@
+package com.chenk.springbean2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author chenk
+ * @create 2020/11/17 14:36
+ */
+@Service
+public class UserService {
+    @Autowired
+    private UserDao userDao;
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+    public String get(String name) {
+        return userDao.get(name);
+    }
+}
